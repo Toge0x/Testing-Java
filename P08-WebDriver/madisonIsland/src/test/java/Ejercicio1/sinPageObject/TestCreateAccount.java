62dcba21a69d03a55daa954c9a734b4797f14468
amsm30-ua <amsm30@gcloud.ua.es>
2025-05-14 22:04:09 +0200

    ejercicio1 hecho

--- P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestCreateAccount.java
+++ P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestCreateAccount.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+
 
 public class TestCreateAccount {
     WebDriver driver;
@@ -15,6 +18,10 @@ public class TestCreateAccount {
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.setBrowserVersion("131");
+        //options.addArguments("--headless=new");   esto siempre quitado para ver el proceso del test
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("http://demo.magento.recolize.com");
@@ -28,26 +35,22 @@ public class TestCreateAccount {
     @Tag("OnlyOnce")
     @Test
     public void S1_srequirement_createAccount_should_create_new_account_in_the_demo_store_when_this_account_does_not_exist(){
-        Assertions.assertEquals("Madison Island", driver.getTitle());   // comprobamos que estamos en la página correcta - Paso 1
-
-        driver.findElement(By.cssSelector("div.account-cart-wrapper a.skip-link.skip-account")).click();    // clickamos el boton del registro
-        driver.findElement(By.cssSelector("div#header-account div.links ul a[title='Log In']")).click();   // clickamos en el botón de login - Paso 2
-        Assertions.assertEquals("Customer Login", driver.getTitle());       // comprobamos que estamos en la página que queremos - Paso 3
-
-        driver.findElement(By.cssSelector("div.buttons-set a[title='Create an Account'")).click();  // clickamos para crear cuenta  - Paso 4
-        Assertions.assertEquals("Create New Customer Account", driver.getTitle());          // Paso 5
-
-        driver.findElement(By.id("firstname")).sendKeys("Antonio");                     // rellenamos los campos excepto confirmation
-        driver.findElement(By.id("lastname")).sendKeys("Martinez Santa");
-        driver.findElement(By.id("email_address")).sendKeys("antoniomartinezua@gmail.com");
-        driver.findElement(By.id("password")).sendKeys("AntonioUA123");
-        driver.findElement(By.cssSelector("div.buttons-set button[title='Register']")).click();     // pulsamos el boton de register - Paso 6
-
-        
-
-
-
-
-
+        Assertions.assertEquals("Madison Island", driver.getTitle());   // PASO 1
+        driver.findElement(By.cssSelector("div.account-cart-wrapper a.skip-link.skip-account")).click();
+        driver.findElement(By.cssSelector("div#header-account div.links ul a[title='Log In']")).click();   // PASO 2
+        Assertions.assertEquals("Customer Login", driver.getTitle());                                      // PASO 3
+        driver.findElement(By.cssSelector("div.buttons-set a[title='Create an Account']")).click(); // PASO 4
+        Assertions.assertEquals("Create New Customer Account", driver.getTitle());                  // PASO 5
+        driver.findElement(By.id("firstname")).sendKeys("Copito");
+        driver.findElement(By.id("lastname")).sendKeys("Linda");
+        driver.findElement(By.id("email_address")).sendKeys("copito1240724@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("coposdenieve");
+        driver.findElement(By.cssSelector("div.buttons-set button[title='Register']")).click();    // PASO 6
+        Assertions.assertEquals("This is a required field.", driver.findElement(By.id("advice-required-entry-confirmation")).getText());     // PASO 7
+        driver.findElement(By.id("confirmation")).sendKeys("coposdenieve");
+        driver.findElement(By.cssSelector("div.buttons-set button[title='Register']")).submit();    // PASO 8
+        Assertions.assertEquals("My Account", driver.getTitle());
+        Assertions.assertEquals("Thank you for registering with Madison Island.", driver.findElement(By.cssSelector("li.success-msg span")).getText());
+        Assertions.assertEquals("Hello, Copito Linda!", driver.findElement(By.cssSelector("div.welcome-msg p.hello")).getText());
     }
 }
---
+++ P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestLogin.java
@@ -0,0 +1,61 @@
+package Ejercicio1.sinPageObject;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class TestLogin {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setBrowserVersion("131");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://demo.magento.recolize.com");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void R2_requirement_loginOK_should_login_with_success_when_user_account_exists() {
+        Assertions.assertEquals("Madison Island", driver.getTitle());       // PASO 1
+        driver.findElement(By.cssSelector("div.account-cart-wrapper a.skip-link.skip-account")).click();
+        driver.findElement(By.cssSelector("div#header-account div.links ul a[title='Log In']")).click();   // PASO 2
+        Assertions.assertEquals("Customer Login", driver.getTitle());       // PASO 3
+        driver.findElement(By.id("email")).sendKeys("copito1240724@gmail.com");
+        driver.findElement(By.id("send2")).click();                         // PASO 4
+        Assertions.assertEquals("This is a required field.", driver.findElement(By.id("advice-required-entry-pass")).getText());    // PASO 5
+        driver.findElement(By.id("pass")).sendKeys("coposdenieve");
+        driver.findElement(By.id("send2")).click();                         // PASO 6
+        Assertions.assertEquals("My Account", driver.getTitle());
+        Assertions.assertEquals("Hello, Copito Linda!", driver.findElement(By.cssSelector("div.welcome-msg p.hello")).getText());   // PASO 7
+    }
+
+    @Test
+    public void R3_requirement_loginFailed_should_fail_when_user_account_not_exists(){
+        Assertions.assertEquals("Madison Island", driver.getTitle());       // PASO 1
+        driver.findElement(By.cssSelector("div.account-cart-wrapper a.skip-link.skip-account")).click();
+        driver.findElement(By.cssSelector("div#header-account div.links ul a[title='Log In']")).click();   // PASO 2
+        Assertions.assertEquals("Customer Login", driver.getTitle());       // PASO 3
+        driver.findElement(By.id("email")).sendKeys("copito1240724@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("coposdenieve1");
+        driver.findElement(By.id("send2")).click();                         // PASO 4
+        Assertions.assertEquals("Invalid login or password.", driver.findElement(By.cssSelector("ul.messages li.error-msg span")).getText());   // PASO 5
+    }
+}
